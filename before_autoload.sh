f299b6d1e9eeb2e98a5474cdfef85165611a783a
kgleizes <kgleizes@202-ecommerce.com>
2022-06-13 13:03:49 +0200

    refs #35337, #35344 SDK correction after composer update (function)

--- before_autoload.sh
+++ before_autoload.sh
@@ -21,4 +21,4 @@ then
     rm -f ./vendor/symfony/polyfill-72/php72.php
     cp -f ./202/compatibilityphp/symfony/polyfill-php72/Php72.php ./vendor/symfony/polyfill-php72/Php72.php
 fi
-sed -ri -e 's!public function enableSanbox!public function enableSandbox!g'
+sed -ri -e 's!public function enableSanbox!public function enableSandbox!g' ./vendor/202ecommerce/younitedpay-sdk/src/Request/AbstractRequest.php
