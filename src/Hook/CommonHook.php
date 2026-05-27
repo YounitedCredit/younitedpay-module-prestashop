ff2dd4d57add22860e50675c099f70b9714e20b6
Alexandre Fayadas 202 <afayadas@202-ecommerce.com>
2026-05-27 16:00:50 +0200

    Apply PHPstan recommendation

--- src/Hook/CommonHook.php
+++ src/Hook/CommonHook.php
@@ -91,8 +91,8 @@ class CommonHook extends AbstractHook
                 break;
             case $controller instanceof \OrderController:
             case $controller instanceof \TheCheckoutModuleFrontController:
-            // @phpstan-ignore-next-line
-            case $controller instanceof \StEasyCheckoutDefaultModuleFrontController:
+            case class_exists('StEasyCheckoutDefaultModuleFrontController', false)
+                && $controller instanceof \StEasyCheckoutDefaultModuleFrontController:
             case $controller instanceof \OnePageCheckoutPSPaymentModuleFrontController:
                 $invoiceAddress = new \Address(Context::getContext()->cart->id_address_invoice);
                 $langId = Context::getContext()->language->id;
